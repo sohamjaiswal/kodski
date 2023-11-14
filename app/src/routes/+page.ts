ec6e99a489a957a8bba118558fe009743aae9098
Soham Jaiswal <sohamjaiswal10@gmail.com>
2023-11-14 11:43:08 +0530

    feat: regen lock file for dependency resolution

--- app/src/routes/+page.ts
+++ app/src/routes/+page.ts
@@ -1,18 +1,12 @@
-import type { Contributions } from "$lib/types"
 import type { PageLoad } from "./$types"
 
-export const load: PageLoad = async({fetch}) => {
-  const contributions = await (await fetch("/api/v1/ghStats/sohamjaiswal")).json() as Contributions
+export const load: PageLoad = async() => {
   try {
     const landingMd = await import("../posts/landing.svx")
     return {
       content: landingMd.default,
-      contributions
     }
   } catch (e) {
     console.log("An error ocurred")
-  }
-  return {
-    contributions
   }
 } 
